00086d43500fda4009375dce301097872b0fb82d
bioly1910 <grabarzmichal@gmail.com>
2023-05-08 15:22:23 +0200

    podlączenie do bazy postgres

--- python/basics/zz ikony, png, pliki testowe/002 copy 2.py
+++ python/basics/zz ikony, png, pliki testowe/002 copy 2.py
@@ -1,55 +1,20 @@
-import tkinter as tk
-from tkinter import ttk
- 
- 
-class Window:
-    def __init__(self, master):
-        self.master = master
- 
-        self.frame= ttk.Frame(self.master)
- 
-        self.progressBar= ttk.Progressbar(self.frame, mode='determinate',
-                                          maximum=200, length=200, orient= tk.VERTICAL)
-        self.progressBar.pack(padx = 10, pady = 10, side = tk.RIGHT)
- 
- 
-        self.button= ttk.Button(self.frame, text= "Reset",
-                                            command= self.reset)
-        self.button.pack(padx = 10, pady = 10, side = tk.BOTTOM)
- 
-        self.button= ttk.Button(self.frame, text= "Print", 
-                                            command= self.display)
-        self.button.pack(padx = 10, pady = 10, side = tk.BOTTOM)
- 
-        self.button= ttk.Button(self.frame, text= "Increase", 
-                                            command= self.increment)
-        self.button.pack(padx = 10, pady = 10, side = tk.BOTTOM)
- 
-        self.button= ttk.Button(self.frame, text= "Stop",
-                                 command= lambda: self.stop())
-        self.button.pack(padx = 10, pady = 10, side = tk.BOTTOM)
- 
-        self.button= ttk.Button(self.frame, text= "Start",
-                                 command= lambda: self.progressBar.start(10))
-        self.button.pack(padx = 10, pady = 10, side = tk.BOTTOM)
-  
-        self.frame.pack(padx = 5, pady = 5)
-         
-    def increment(self):
-        self.progressBar.step(20)
- 
-    def display(self):
-        print(self.progressBar["value"])
- 
-    def stop(self):
-        val = self.progressBar["value"]
-        self.progressBar.stop()
-        self.progressBar["value"] = val
- 
-    def reset(self):
-        self.progressBar["value"] = 0
- 
- 
-root = tk.Tk()
-window = Window(root)
-root.mainloop()
+import psycopg2
+
+#establishing the connection
+conn = psycopg2.connect(
+   database="postgres", user='postgres', password='password', host='127.0.0.1', port= '5432'
+)
+conn.autocommit = True
+
+#Creating a cursor object using the cursor() method
+cursor = conn.cursor()
+
+#Preparing query to create a database
+sql = '''CREATE database mydb''';
+
+#Creating a database
+cursor.execute(sql)
+print("Database created successfully........")
+
+#Closing the connection
+conn.close()
